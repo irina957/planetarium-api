6b1095bd9853eaff4fe21ffb899ccc74cdfd7395
iryna <irakirvas78@gmail.com>
2026-01-27 14:10:13 +0200

    feat: add TicketInLine to ReservationAdmin

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -4,9 +4,17 @@ from planetarium.models import (ShowTheme, AstronomyShow,
                                 PlanetariumDome, ShowSession,
                                 Reservation, Ticket)
 
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = [TicketInLine,]
+
+
 admin.site.register(ShowTheme)
 admin.site.register(AstronomyShow)
 admin.site.register(PlanetariumDome)
 admin.site.register(ShowSession)
-admin.site.register(Reservation)
 admin.site.register(Ticket)
