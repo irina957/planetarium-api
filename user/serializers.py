1e02354183d94d29cacf5dd8f49e401a3e2ad5c9
iryna <irakirvas78@gmail.com>
2026-02-01 16:32:31 +0200

    feat: implement ManageUserView for profile management

--- user/serializers.py
+++ user/serializers.py
@@ -5,13 +5,37 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff",)
-        read_only_fields = ("id", "is_staff",)
-        extra_kwargs = {"password": {"write_only": True,
-                                     "min_length": 5,
-                                     "style": {"input_type": "password", },
-                                     "label": "Password"}}
+        fields = (
+            "id",
+            "username",
+            "email",
+            "password",
+            "is_staff",
+        )
+        read_only_fields = (
+            "id",
+            "is_staff",
+        )
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5,
+                "style": {
+                    "input_type": "password",
+                },
+                "label": "Password",
+            }
+        }
 
     def create(self, validated_data):
         """Create user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update User with encrypted password"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
